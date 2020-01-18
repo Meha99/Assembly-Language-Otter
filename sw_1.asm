8a3eef5980ab73b0d94cfcac61cd37976bd11919
Meha99 <45151702+Meha99@users.noreply.github.com>
2020-01-18 12:55:18 -0800

    Update sw_1.asm

--- sw_1.asm
+++ sw_1.asm
@@ -1,3 +1,5 @@
+#Read 3 inputs from address 0x10010000, add them together, and output the result to address 0x10010020. 
+#Assume the input values are 16-bit unsigned values.
 main:   lhu x1 0x10010000
 	lhu x2 0x10010004
 	lhu x3 0x10010008
